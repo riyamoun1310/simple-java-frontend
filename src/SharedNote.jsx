10e884ff07395cab447d37c215b4606f2929d501
hp <hp@example.com>
2025-09-03 22:41:06 +0530

    Finalize all frontend fixes and resolve conflicts

--- src/SharedNote.jsx
+++ src/SharedNote.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 import { Card, CardContent, Typography, Box } from '@mui/material';
 
 export default function SharedNote({ uuid }) {
